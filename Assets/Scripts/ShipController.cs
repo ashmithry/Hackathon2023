e1c452e410de3916ae3daf316c7f529faed7a22c
pixel1l <66034054+ashmithry@users.noreply.github.com>
2023-05-20 10:07:40 -0400

    shipcontroller initial

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,18 +1,35 @@
+using System.Reflection.Metadata;
+using Microsoft.CSharp.RuntimeBinder;
+using System.Security.Cryptography;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    [Header("Values")]
+    public int xInput, yInput;
+    public float movementSpeed;
+
+    [Header("References")]
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void void FixedUpdate()
+    {
+        rb
     }
 }
