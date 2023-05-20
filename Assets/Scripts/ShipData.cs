7869cea56b476007fb08cb18c147705d7b74dfad
pixel1l <66034054+ashmithry@users.noreply.github.com>
2023-05-20 16:23:59 -0400

    COin

--- Assets/Scripts/ShipData.cs
+++ Assets/Scripts/ShipData.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,50 @@ using UnityEngine;
 public class ShipData : MonoBehaviour
 {
     [Header("Data")]
+    public ShipController controller;
+    public ShipCombat combat;
+
     public float health = 50f;
+    public int upgradePoints = 0;
+
+    public int coins;
+    public float coinsToUpgrade = 10;
+
+
+    public float speed = 100f;
+
+    public float regen = 0.25f;
+    public float timeToRegen = 5f;
+    private float regenTimer;
+    
+
+    public float damage = 7f;
+    public int cannons = 1;
+    public float fireRate = 1.5f;
+
 
 
     void Start()
     {
-        
+        controller = GetComponent<ShipController>();
+        controller.movementSpeed = speed;
+        controller.turnSpeed = speed;
+
+        combat = GetComponent<ShipCombat>();
+        combat.cooldown = fireRate;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(coins >= coinsToUpgrade)
+        {
+            upgradePoints += 1;
+            coinsToUpgrade =  Mathf.Floor(coinsToUpgrade *= 1.1f);
+            coins = 0;
+        }
     }
 
     public void Damage(float dmg)
@@ -34,4 +67,38 @@ public class ShipData : MonoBehaviour
         //delete player
         Destroy(gameObject);
     }
+
+    public void Upgrade(string stat)
+    {
+        if(upgradePoints > 0)
+        {
+            switch(stat)
+            {
+                case "cannons":
+                    cannons += 1;
+                    break;
+                case "damage":
+                    damage += 1.5f;
+                    break;
+                case "fire rate":
+                    fireRate -= 0.25f;
+                    combat.cooldown = fireRate;
+                    break;
+                case "regen":
+                    regen += 0.25f;
+                    break;
+                case "speed":
+                    speed += 10;
+                    controller.movementSpeed = speed;
+                    controller.turnSpeed = speed;
+                    break;
+                default:
+                    break;
+
+            }
+
+            upgradePoints -= 1;
+        }
+    }
+
 }
