cff2c727ad8ea5996bb670df331449e05d15fb05
2900xt <115376060+2900xt@users.noreply.github.com>
2023-05-21 01:44:16 -0400

    Sound

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,33 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using System.Collections;
+
+[System.Serializable]
+
+public class Sound
+{
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
